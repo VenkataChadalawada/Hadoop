46c1efaa3fdb0a2a7b05d433ec9333a31bb8e7e2
Venkata Chadalawada <venchad@gmail.com>
2018-01-17 23:25:33 -0600

    Update hive_example.py

--- hive_example.py
+++ hive_example.py
@@ -6,3 +6,17 @@ ORDER BY ratingCount DESC;
 
 SELECT n.title, ratingCount
 FROM topMovieIDs t JOIN names n ON t.movieID = n.movieID;
+
+# create view if not exist
+
+CREATE VIEW IF NOT EXISTS topmovieIDs AS
+SELECT movieID, count(movieID) as ratingCount
+FROM u
+GROUP BY movieID
+ORDER BY ratingCount DESC;
+
+SELECT n.title, ratingCount
+FROM topMovieIDs t JOIN names n ON t.movieID = n.movieID;
+
+#TO DELETE A VIEW
+DROP VIEW topmovieIDs
